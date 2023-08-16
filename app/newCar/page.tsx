d621fa8880972de5f547a02d4bbc2c93abec0b40
rahil-f <felixrahil@yahoo.fr>
2023-08-16 12:08:58 +0200

    fix(newCar): use the good set state for plate

--- app/newCar/page.tsx
+++ app/newCar/page.tsx
@@ -162,7 +162,7 @@ const NewCar = () => {
                                 <label htmlFor="plate" className="block text-sm font-medium leading-6 text-gray-900">Plaque</label>
                                 <div className="mt-2">
                                     <div className="mt-2">
-                                        <input onChange={(e) => setImgCar(e.target.value)} id="plate" name="plate" type="text" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6" />
+                                        <input onChange={(e) => setPlate(e.target.value)} id="plate" name="plate" type="text" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6" />
                                     </div>
                                 </div>
                             </div>
